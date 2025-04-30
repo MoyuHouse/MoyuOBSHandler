3fc8c84594f663868d9dea4188df7329c9097c0f
Rinsan <cyjdtxz@gmail.com>
2025-04-30 17:22:54 +0800

    feat: 使用 tornado 框架解决并发问题 (#2)
    * feat: 使用 tornado 框架处理请求
    
    * chores: 忽略配置文件
    
    * fix: 调整引用和 yaml 读取配置逻辑
    
    * Create pylint.yml
    
    * fix: 修正静态代码检查问题
    
    * style: 修正静态代码检查问题
    
    * style: 忽略检查问题，不对子类做抽象方法复写检查

--- obs_event_handler.py
+++ obs_event_handler.py
@@ -1,34 +1,186 @@
+"""
+    Author: @DZDcyj
+    This module provides the basic structure of the http server for obs monitor
+"""
+
 import json
-from http.server import HTTPServer, BaseHTTPRequestHandler
+import subprocess
+import uuid
+from typing import Any
+from urllib.parse import unquote
 
-sample_data = {'result': 'HTTP SERVER OK'}
-host = ('0.0.0.0', 1919)
+import tornado.httpclient
+import tornado.ioloop
+import tornado.options
+import tornado.web
+import yaml
+from tornado import gen, concurrent
+from tornado.httputil import HTTPServerRequest
+from tornado.options import define, options
+from tornado.web import Application
+
+define("port", default=1919, help="run on the given port", type=int)
+
+HTTP_ACCEPT = 202
+HTTP_BAD_REQUEST = 400
 
 
-class OBSHandleServer(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        # 发给请求客户端的响应数据
-        self.send_header('Content-type', 'application/json')
-        self.end_headers()
-        self.wfile.write(json.dumps(sample_data).encode())
+def check_data(data) -> bool:
+    """
+    This function check if the data is valid
+    :param data: The post request data
+    :return: If the data is valid
+    """
+    if data:
+        json_data = json.loads(data)
+        if json_data:
+            return "subject" in json_data and json_data["subject"] != ""
+    return False
 
-    def do_POST(self):
-        self.send_response(200)
 
-        datas = self.rfile.read(int(self.headers['content-length']))
-        print('headers', self.headers)
-        print("-->> post:", self.path, self.client_address)
-        print(datas)
+# pylint: disable=W0223
+class OBSEventHandler(tornado.web.RequestHandler):
+    """
+    The OBS HTTP Server Class
+    """
+    executor = concurrent.futures.ThreadPoolExecutor(max_workers=10)
 
-        # 发给请求客户端的响应数据
-        self.send_header('Content-type', 'application/json')
-        self.end_headers()
-        self.wfile.write(json.dumps(sample_data).encode())
+    def __init__(self, application: "Application", request: HTTPServerRequest, **kwargs: Any):
+        """
+        The OBS HTTP Server Class initialization.
+        :param application: Inherit from super class
+        :param request: Inherit from super class
+        :param kwargs: Inherit from super class
+        """
+        super().__init__(application, request, **kwargs)
+        with open('config/config.yaml', 'r', encoding='utf-8') as stream:
+            config = yaml.safe_load(stream)
+            self.addons_path = config['l4d2server']['addons_path']
+            self.temp_path = config['l4d2server']['temp_path']
+            self.obs_bucket = config['l4d2server']['obs_bucket']
+
+    def archive_file_handler(self, file_path):
+        """
+        Handle the Archive File
+        :param file_path: The archive file path
+        This function unzip the archive file of zip rar and 7z
+        """
+        orig_file_name = file_path.split('/')[-1]
+        # 获取类型
+        # 规范来讲应该通过文件头等方式确认，但考虑到内部使用就不做额外验证了
+        suffix = orig_file_name.split('.')[-1]
+        unzip_tool = ''
+        params = ''
+        if suffix == 'zip':
+            unzip_tool = 'unzip'
+            params = '-o'
+        elif suffix == 'rar':
+            unzip_tool = 'unrar'
+            params = 'e -o+'
+        elif suffix == '7z':
+            unzip_tool = '7za'
+            params = 'e -aoa'
+        else:
+            print('Not Archive! Skipping...')
+            return
+
+        print(subprocess.call(
+            f'cd {self.temp_path} && {unzip_tool} {params} {orig_file_name}',
+            shell=True))
+
+    def send_400_response(self):
+        """
+        function that return 400 response
+        """
+        bad_ret = {
+            "code": HTTP_BAD_REQUEST,
+            "data": {
+                "msg": "Required field not in request body!",
+            }
+        }
+        self.set_status(400)
+        self.write(json.dumps(bad_ret))
+        self.set_header('Content-Type', 'application/json')
+
+    @gen.coroutine
+    def post(self):
+        """
+        Main Function that handle the post requests
+        """
+        if not check_data(self.request.body):
+            self.send_400_response()
+            return
+        print(self.request.remote_ip)
+        print(unquote(self.request.body.decode('utf-8')))
+        success_ret = {
+            "code": HTTP_ACCEPT,
+            "data": {
+                "msg": "create job success!",
+                "job_id": uuid.uuid4().hex,
+            }
+        }
+        self.handle_zip_file(self.request.body)
+        self.set_status(202)
+        self.write(json.dumps(success_ret))
+        self.set_header('Content-Type', 'application/json')
+
+    @concurrent.run_on_executor
+    def handle_zip_file(self, datas):
+        """
+        This function handle the main process of archives handling
+        :param datas: The post request body
+        """
+        # datas 为 bytes
+        req = json.loads(datas)
+        obs_orig_file = req['subject']
+        # 解码 HTTP 编码
+        obs_file = unquote(obs_orig_file)
+        print(f'{obs_file} upload detected!')
+        print(f'Downloading {obs_file}...')
+        result = subprocess.call(['obsutil', 'cp', f'obs://{self.obs_bucket}/{obs_file}', self.temp_path])
+        if result != 0:
+            print(f'Error: {result}')
+
+        print(subprocess.call(['ls', self.temp_path]))
+
+        # Unzip
+        print('Handle the Archive...')
+        self.archive_file_handler(obs_file)
+        print('Unpack Step Complete!')
+
+        print(subprocess.call(['ls', self.temp_path]))
+
+        # Move VPKs
+        print('Moving VPKs...')
+        vpks = subprocess.run(
+            f'ls {self.temp_path} | grep vpk',
+            shell=True, capture_output=True, check=False).stdout
+        print('vpks:\n', vpks.decode('utf-8'))
+        vpk_files = vpks.decode('utf-8').strip().split('\n')
+        subprocess.call(
+            f'mv {self.temp_path}/*.vpk {self.addons_path}',
+            shell=True)
+        print('Moved VPKs!')
+        for vpk_file in vpk_files:
+            chk_rst = subprocess.run(
+                f'ls {self.addons_path}"{vpk_file}"',
+                shell=True, capture_output=True, check=False)
+            print(chk_rst.stdout.decode('utf-8'))
+
+
+urls = [(r'/', OBSEventHandler), ]
+
+
+def main():
+    """
+    The main function, starts the server
+    """
+    tornado.options.parse_command_line()
+    print('Listen at port:', options.port)
+    app = tornado.web.Application(urls, debug=True)
+    app.listen(options.port)
+    tornado.ioloop.IOLoop.current().start()
 
 
 if __name__ == '__main__':
-    server = HTTPServer(host, OBSHandleServer)
-    print("Server Start Listening at: %s:%s" % host)
-
-    server.serve_forever()
+    main()
